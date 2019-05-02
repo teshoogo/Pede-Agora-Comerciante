d61c51442a4568ad833f85123e27fb8e1fd29de1
weslleyth15 <50183367+weslleyth15@users.noreply.github.com>
2019-05-01 22:10:25 -0300

    Atualização

---
+++ www/js/config.js
@@ -0,0 +1,7 @@
+
+var krms_config ={					
+	'ApiUrl':"http://pedeagora.hospteste.ga/merchantapp/api",
+	'DialogDefaultTitle':"Notificação",
+	'APIHasKey':"o654asd4sadsfs879fsd3v21xczvc8as7dsda5f45s6ad4f65asdf87g",
+	'debug': false
+};
